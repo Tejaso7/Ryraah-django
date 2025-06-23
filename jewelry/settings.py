1506887014853023e0d59f6dcafd58290768e02c
Tejas Sawant <tejusawant302@gmail.com>
2025-06-23 19:36:44 +0530

    Update settings.py

--- jewelry/settings.py
+++ jewelry/settings.py
@@ -12,6 +12,9 @@ load_dotenv()
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
+# Celery configuration
+CELERY_BROKER_URL = 'amqp://guest:guest@rabbitmq:5672//'
+
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
